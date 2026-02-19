0dcfa2b2f65bf49ade7cfcda3af3afa649a10a37
adiisde <adityaa.sde@gmail.com>
2026-02-19 22:21:48 +0530

    chore: initialize ledger-core spring boot service

---
+++ ledger-core/src/test/java/com/ledger/core/LedgerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledger.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
